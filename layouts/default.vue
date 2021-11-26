16dc9c4e14849191fe0730eebce0d6df48c995d2
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-11-25 23:30:17 -0800

    fancy transitions

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="min-h-screen w-screen bg-gray-100">
-    <nav class='sticky-top py-3 bg-gray-100 flex flex-col sm:flex-row'>
+    <nav class='sticky-top py-6 sm:py-3 bg-gray-100 flex flex-col sm:flex-row'>
       <a class='mx-auto flex flex-col  sm:flex-row sm:flex-grow' >
-        <menu-icon class="absolute sm:hidden top-6 left-8"/>
+        <menu-icon @click="menu = !menu" class="absolute sm:hidden top-6 left-8"/>
         <img class='h-8 mx-auto my-auto sm:mr-3 sm:ml-5 w-8' src="/icon.png" /> 
-      <span class="text-red-700  my-2 block text-2xl font-bold tracking-wide">Gunn Math Competition</span>
+      <span  class="text-red-700  my-2 block text-2xl font-bold tracking-wide">Gunn Math Competition</span>
       </a>
-      <div class="flex sm:justify-end sm:my-auto flex-col sm:flex-grow sm:flex-row">
-      <a class="font-mono sm:my-auto block mx-auto sm:mx-5 text-center sm:text-right text-black hover:text-red-700 hover:underline" :key='link' :href="link" v-for="link of ['/','/schedule','/staff']">
+      <div :class="menu?'scale-y-100':'scale-y-0 sm:scale-y-100'" class="flex sm:justify-end transition-transform duration-200 transform overflow-hidden origin-top sm:my-auto flex-col sm:flex-grow sm:flex-row">
+      <a class="font-mono sm:my-auto block mx-auto sm:mx-5 text-center  sm:text-right text-black hover:text-red-700 hover:underline" :key='link' :href="link" v-for="link of ['/','/schedule','/staff']">
         {{link}}
       </a>
       </div>
@@ -27,6 +27,9 @@ import { MenuIcon } from 'vue-feather-icons'
 export default {
   components: {
     MenuIcon
-  }
+  }, 
+  data: ()=>({
+    menu: false,
+  })
 }
 </script>
