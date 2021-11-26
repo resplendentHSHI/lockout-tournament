813d5dcec248847ef8971b00133960eec3c2dc6c
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2021-11-25 21:22:22 -0800

    header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,27 @@
 <template>
-  <div>
+  <div class="min-h-screen w-screen bg-gray-100">
+    <nav class='sticky-top py-3 bg-gray-200 flex flex-col sm:flex-row'>
+      <a class='mx-auto flex flex-col' href="/">
+        <menu-icon class="absolute top-6 left-8"/>
+        <img class='h-12 mx-auto w-12' src="/icon.png" /> 
+      <span class="text-red-900 my-2 block text-2xl font-bold tracking-wide">Gunn Math Competition</span>
+      </a>
+    </nav>
     <Nuxt />
   </div>
 </template>
 
 <style>
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 </style>
+<script>
+import { MenuIcon } from 'vue-feather-icons'
+
+export default {
+  components: {
+    MenuIcon
+  }
+}
+</script>
