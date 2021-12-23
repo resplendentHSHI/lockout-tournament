6f45f526c7d6991d6774fac1e1123bf27e8a5a9f
Saumya Singhal <65318685+saumyasinghal747@users.noreply.github.com>
2021-12-22 23:33:42 -0800

    cleaning up headings

--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,13 @@
   <div class="prose dark:prose-dark prose-red mx-auto px-5 max-w-sm  sm:max-w-xl md:max-w-5xl">
     <h1 class="">{{doc.title}}</h1>
     <nuxt-content  :document="doc" />
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3" >
-      <a :href="sponsor.link" class="block bg-white dark:bg-gray-700 rounded-md p-8" :key="sponsor.name" v-for="sponsor of sponsors">
+  </div>
+  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mx-auto px-5 max-w-sm  sm:max-w-xl md:max-w-5xl" >
+      <a :href="sponsor.link" class="block mx-3 my-3 bg-white dark:bg-gray-700 rounded-md p-6" :key="sponsor.name" v-for="sponsor of sponsors">
         <h1 class="text-red font-lg font-semibold tracking-wide">{{sponsor.name}}</h1>
         <img class="h-24 w-auto" :src="sponsor.path"></img>
       </a>
     </div>
-  </div>
 </template>
 
 <script>
