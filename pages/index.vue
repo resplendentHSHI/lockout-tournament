a8eddaa84e1c2a29bb986bb7e10a49870caaaca1
Saumya Singhal <65318685+saumyasinghal747@users.noreply.github.com>
2022-03-05 01:37:53 -0800

    center images

--- pages/index.vue
+++ pages/index.vue
@@ -7,8 +7,8 @@
   <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mx-auto px-5 max-w-sm  sm:max-w-xl md:max-w-5xl" >
       <a target="_blank" :href="sponsor.link" class="block mx-3 my-3 bg-white dark:bg-gray-600 shadow-md hover:shadow-2xl rounded-md px-6 py-4" :key="sponsor.name" v-for="sponsor of sponsors">
         <h1 class="text-red mb-2 transition-shadow duration-300 font-xl font-semibold tracking-wide">{{sponsor.name}}</h1>
-        <img class="dark:hidden block  max-h-24 my-auto h-auto w-auto" :src="sponsor.light_image"></img>
-        <img class="hidden dark:block max-h-24 my-auto h-auto w-auto" :src="sponsor.dark_image"></img>
+        <img class="dark:hidden block  max-h-24 my-auto mx-auto h-auto w-auto" :src="sponsor.light_image"></img>
+        <img class="hidden dark:block max-h-24 my-auto mx-auto h-auto w-auto" :src="sponsor.dark_image"></img>
       </a>
     </div>
 
